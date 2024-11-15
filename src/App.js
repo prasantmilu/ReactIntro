3e781dbe0ad61c0f5e8d607610719cb5fa799604
prasantmilu <prasant@ivy-baron.com>
2024-11-15 12:40:42 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <h1>Welcome to fisrt <b style={{color:'red'}}>REACT JS</b> Page.</h1>
+     
+    </div>
+  );
+}
+
+export default App;
